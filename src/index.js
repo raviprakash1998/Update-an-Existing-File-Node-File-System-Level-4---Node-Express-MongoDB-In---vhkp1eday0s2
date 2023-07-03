31562bf99bbe0d576361ca4fbc15326b01275f46
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-07-04 03:22:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  fs.appendFile(fileName, fileContent);
 };
 
 module.exports = updateFile;
